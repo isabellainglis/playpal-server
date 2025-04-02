06e9d0c28049e9e49023e266b3e67014e0cd4c08
issy-inglis@hotmail.co.uk <issy-inglis@hotmail.co.uk>
2025-04-02 17:23:07 +0100

    remove unnecessary song chords api call

--- controllers/chordControllers.js
+++ controllers/chordControllers.js
@@ -16,20 +16,4 @@ const getChordData = async (_req, res) => {
   }
 };
 
-const getAllSongChords = async (_req, res) => {
-  const sql = `SELECT name, song_id, id FROM chord;`;
-
-  try {
-    const [results] = await connection.query(sql);
-
-    if (!results.length) {
-      res.status(400).json({ msg: "No chords in DB" });
-    }
-
-    res.json(results);
-  } catch (error) {
-    res.status(500).json(error.message);
-  }
-};
-
-export { getChordData, getAllSongChords };
+export { getChordData };
--- routes/chordRoutes.js
+++ routes/chordRoutes.js
@@ -1,13 +1,8 @@
 import express from "express";
-import {
-  getAllSongChords,
-  getChordData,
-} from "../controllers/chordControllers.js";
+import { getChordData } from "../controllers/chordControllers.js";
 
 const router = express.Router();
 
 router.get("/", getChordData);
-
-router.get("/all", getAllSongChords);
 
 export default router;
