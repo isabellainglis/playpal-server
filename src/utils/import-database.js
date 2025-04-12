8d13aa0544a2c25b84ce77b7d0e4bdb0ac235bea
issy-inglis@hotmail.co.uk <issy-inglis@hotmail.co.uk>
2025-04-12 19:22:00 +0100

    cleanupimporting db

--- src/utils/import-database.js
+++ src/utils/import-database.js
@@ -2,11 +2,13 @@ import connection from "./mysql.js";
 import fs from "fs";
 
 try {
-  const sql = fs.readFileSync("./db/database.sql").toString();
+  const sql = fs.readFileSync("./db/database.sql", "utf8");
   await connection.query(sql);
   console.log("Database imported");
 } catch (error) {
   console.error(`Database import failed: ${error}`);
 }
+
+await connection.end();
 
 process.exit();
